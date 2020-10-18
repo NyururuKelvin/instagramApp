b162cffd6d203dfa22827ee58b98e00e5f551752
Nyururukelvin <nyururukelvin9@gmail.com>
2020-10-18 21:40:40 +0300

    my commit add methods & make migrations

--- ig/models.py
+++ ig/models.py
@@ -1,15 +1,34 @@
 from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
+import django.utils.timezone
 
 # Models
 class Post(models.Model):
     image = models.ImageField(upload_to = 'posts/', default='No Image')
-    name = models.CharField(max_length=60)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default='')
     caption = models.CharField(max_length=60)
     pub_date = models.DateTimeField(auto_now_add=True)
     likes = models.IntegerField(default=0)
 
     def save_post(self):
         self.save()
+
+    def delete_post(self):
+        self.delete()
+
+    class Meta:
+        ordering = ['pub_date']
+
+    @classmethod
+    def update_post(self, update):
+        self.post = update
+        self.save
+
+    @classmethod
+    def display_user_post(cls):
+        post = cls.objects.filter()
+        return post
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE,)
@@ -22,8 +41,35 @@ class Comment(models.Model):
         return comments
 
 class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default='')
     photo = models.ImageField(upload_to = 'posts/', default='No Image')
     bio = models.TextField(max_length=255)
 
     def save_profile(self):
         self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def updateProfile(self, update):
+       self.bio = update
+       self.save
+
+    @classmethod
+    def search_profile(cls, search_term):
+        user = cls.objects.filter(user__username__icontains=search_term)
+        return user 
+
+    @classmethod
+    def get_by_id(cls, id):
+        profile = cls.objects.get(id=id)
+        return profile
+
+class Follow(models.Model):
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following', default='')
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower', default='')
+
+    def __str__(self):
+        return '{} follows {}'.format(self.following,self.follower)
+
+User.add_to_class('followings',models.ManyToManyField('self',through=Follow,related_name='followers',symmetrical=False))
