6b080ffff163340593c135e0ed9f3fc33d4fb9d1
Nyururukelvin <nyururukelvin9@gmail.com>
2020-10-18 16:56:38 +0300

    my commit add register & login code

--- ig/views.py
+++ ig/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http  import HttpResponse, Http404,HttpResponseRedirect
 from .models import Post, Comment, Profile
 from .email import send_welcome_email
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
