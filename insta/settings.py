a33f3586ca374cedbda6e4146e7913d2a9e4c177
Nyururukelvin <nyururukelvin9@gmail.com>
2020-10-18 13:38:57 +0300

    my commit  add templates

--- insta/settings.py
+++ insta/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ig.apps.IgConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
